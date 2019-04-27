36731c22400bd74bf4ad63ff36c779d8fbabb7a1
telmogarcia <telmogarcia@mbp-de-telmo-2.lan>
2019-04-27 16:33:49 +0100

    ViewCasas e ViewUsers

---
+++ src/main/java/pt/teste/testeapp/DefaultView.java
@@ -0,0 +1,13 @@
+package pt.teste.testeapp;
+
+import com.vaadin.navigator.View;
+import com.vaadin.ui.Composite;
+import com.vaadin.ui.Label;
+
+public class DefaultView extends Composite implements View{
+
+    public DefaultView() {
+        setCompositionRoot(new Label("This is the default view"));
+    }
+	
+}
---
+++ src/main/java/pt/teste/testeapp/ViewEventos.java
@@ -0,0 +1,60 @@
+package pt.teste.testeapp;
+
+import com.vaadin.navigator.View;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Composite;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+
+public class ViewEventos extends Composite implements View {
+
+    public ViewEventos() {
+        
+    	 final VerticalLayout layout = new VerticalLayout();
+
+         Button buttonNovo = new Button("Novo");
+         buttonNovo.addClickListener(e -> {
+             
+         });
+         
+         Button buttonEditar = new Button("Editar");
+         buttonEditar.addClickListener(e -> {
+             
+         });
+
+         Button buttonEliminar = new Button("Eliminar");
+         buttonEliminar.addClickListener(e -> {
+             
+         });
+         
+         Evento evento = new Evento();
+         
+         evento.setName("Telmo");
+         evento.setDateStart("23-03-2019");
+         evento.setDateEnd("23-03-2019");
+         evento.setDescription("asd");
+         evento.setType("qwe");
+         
+         Grid<Evento> grid = new Grid<>(Evento.class);
+         
+         grid.addColumn(Evento::getDateStart)
+ 			.setCaption("Date Start");
+         grid.addColumn(Evento::getDateEnd)
+ 			.setCaption("Date End");
+         grid.addColumn(Evento::getDescription)
+ 			.setCaption("Description");
+         grid.addColumn(Evento::getType)
+ 			.setCaption("Type");
+     
+         grid.setItems(evento);
+         
+         
+         layout.addComponents(buttonNovo, buttonEditar, buttonEliminar, grid);
+         
+         setCompositionRoot(layout);
+    	
+    }
+}
